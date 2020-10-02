383992082ffbcfd3aa0998a4dac517a0e5395958
CharlesWilmart <chwilmart@gmail.com>
2020-10-02 18:49:11 +0200

    Added Firebase config
    Firebase will be used for login & auth

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFU6-H7IOaNTp6VTNOlj3BeQo87LIuTR4",
+  authDomain: "whatsapp-mern-dd6bf.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-dd6bf.firebaseio.com",
+  projectId: "whatsapp-mern-dd6bf",
+  storageBucket: "whatsapp-mern-dd6bf.appspot.com",
+  messagingSenderId: "64785736890",
+  appId: "1:64785736890:web:ef99f19154746abf85ce83",
+  measurementId: "G-JG69MHPRQS",
+};
