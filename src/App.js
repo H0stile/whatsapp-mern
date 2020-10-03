5e9962ba2481e384f9f3d6f5bfe4cf1d9b8dd76e
CharlesWilmart <chwilmart@gmail.com>
2020-10-03 20:51:55 +0200

    Setting up the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a whatsapp clone with MERN stack !</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chat component */}
+      <Chat />
     </div>
   );
 }
